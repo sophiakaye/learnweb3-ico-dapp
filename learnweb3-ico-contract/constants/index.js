8e5c7b11a4b13853fd971833889bbf1b51d38362
Sophia KAYA <sophiakaya@Sophias-MacBook-Pro.local>
2022-10-05 12:23:26 +0300

    first commit

---
+++ learnweb3-ico-contract/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x3c00D59a4d8319383848b4760aC6b0c03f5aCbc6";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
